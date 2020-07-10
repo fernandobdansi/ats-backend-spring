371515ce3bed12af36cc4ea5476e16a307b652d0
FBUENOD <fernandobuenodansi@gmail.com>
2020-07-09 22:37:19 -0300

    update

--- src/main/java/edu/ifes/ci/si/les/ats/AtsApplication.java
+++ src/main/java/edu/ifes/ci/si/les/ats/AtsApplication.java
@@ -2,12 +2,28 @@ package edu.ifes.ci.si.les.ats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AtsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtsApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+					.allowedOrigins("http://localhost:8080").allowedOrigins("https://fbuenod.github.io/ats-frontend-vue");
+				
+			}
+		};
 	}
 
 }
