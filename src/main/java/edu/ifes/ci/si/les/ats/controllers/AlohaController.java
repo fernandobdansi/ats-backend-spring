8e0bf34c933ac8fdc3b48bd8a2242f8f96b11f96
FBUENOD <fernandobuenodansi@gmail.com>
2020-08-07 00:10:51 -0300

    Create AlohaController.java

---
+++ src/main/java/edu/ifes/ci/si/les/ats/controllers/AlohaController.java
@@ -0,0 +1,17 @@
+package edu.ifes.ci.si.les.ats.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/")
+public class AlohaController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String hello() {
+		return "Hello! The Service Is Working!";
+	}
+
+}
