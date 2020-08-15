85d186dace04bdc13a439049e331f5fb0db8177e
FBUENOD <fernandobuenodansi@gmail.com>
2020-08-15 20:29:40 -0300

    Add Regras de Negocio

--- src/main/java/edu/ifes/ci/si/les/ats/repositories/ClienteRepository.java
+++ src/main/java/edu/ifes/ci/si/les/ats/repositories/ClienteRepository.java
@@ -1,10 +1,19 @@
 package edu.ifes.ci.si.les.ats.repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import edu.ifes.ci.si.les.ats.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+	@Transactional(readOnly = true)
+	@Query(value = "SELECT CLIENTE.*  FROM ordem_de_servico INNER JOIN dispositivo ON ordem_de_servico.dispositivo_id = dispositivo.id INNER JOIN cliente on dispositivo.cliente_id = cliente. id where ordem_de_servico.pagamento = 'Nao'", nativeQuery = true)
+	public Collection<Cliente> findDevedores();
 
 }
--- src/main/java/edu/ifes/ci/si/les/ats/services/OrdemDeServicoService.java
+++ src/main/java/edu/ifes/ci/si/les/ats/services/OrdemDeServicoService.java
@@ -8,9 +8,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import edu.ifes.ci.si.les.ats.model.Cliente;
 import edu.ifes.ci.si.les.ats.model.OrdemDeServico;
 import edu.ifes.ci.si.les.ats.model.OrdemServicoItem;
+import edu.ifes.ci.si.les.ats.repositories.ClienteRepository;
 import edu.ifes.ci.si.les.ats.repositories.OrdemDeServicoRepository;
+import edu.ifes.ci.si.les.ats.services.exceptions.BusinessRuleException;
 import edu.ifes.ci.si.les.ats.services.exceptions.DataIntegrityException;
 import edu.ifes.ci.si.les.ats.services.exceptions.ObjectNotFoundException;
 
@@ -19,6 +22,8 @@ public class OrdemDeServicoService {
 
 	@Autowired
 	private OrdemDeServicoRepository ordemServicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	public OrdemDeServico findById(Integer id) {
 		OrdemDeServico obj = ordemServicoRepository.findById(id).get();
@@ -36,14 +41,17 @@ public class OrdemDeServicoService {
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public OrdemDeServico insert(OrdemDeServico obj) {
 		try {
-			obj.setId(null);
-			for (OrdemServicoItem item : obj.getOrdemServicosItem()) {
-				item.setOrdemDeServico(obj);
+			if (verificarRegrasDeNegocio(obj)) {
+				obj.setId(null);
+				for (OrdemServicoItem item : obj.getOrdemServicosItem()) {
+					item.setOrdemDeServico(obj);
+				}
+				return ordemServicoRepository.save(obj);
 			}
-			return ordemServicoRepository.save(obj);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Campo(s) obrigatório(s) do OrdemDeServico não foi(foram) preenchido(s)");
 		}
+		return null;
 	}
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
@@ -67,6 +75,27 @@ public class OrdemDeServicoService {
 			ordemServicoRepository.flush();
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir um OrdemDeServico!");
+		}
+	}
+
+	public boolean verificarRegrasDeNegocio(OrdemDeServico obj) {
+
+		// Regra de Negócio 1: Cliente não pode ter Ordens não pagas
+		Collection<Cliente> devedores = clienteRepository.findDevedores();
+		boolean clienteDevedor = false;
+		for (Cliente devedor : devedores) {
+			if (devedor.getId() == obj.getDispositivo().getCliente().getId()) {
+				clienteDevedor = true;
+			}
+		}
+		if (clienteDevedor) {
+			throw new BusinessRuleException("Este cliente deve Ordens de Serviço anteriores!");
+		}
+
+		if (!clienteDevedor) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
